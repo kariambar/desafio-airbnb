e2ad27ef6e5c8f1f89ee3f3b512d80dbf9bc3a3f
maribel <maribel.diaz.j@gmail.com>
2017-08-08 13:30:28 -0400

    Seccion nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,19 @@
-console.log("funciona main.js");
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-});
+//js first-filter//
+    $(document).ready(function(){
+var slider = document.getElementById('test-slider');
 
-/*cambia saludo al hacer hover*/
-$(document).ready(function(){
-/*nav - cambio de opacidad*/
-	$(window).scroll(function(){
-		scroll = $(window).scrollTop();
-		if(scroll > 200){
-			$("#estiloNav").css({"background-color": "#f7f7f7"});
-		}else{
-			$("#estiloNav").css({"background-color": "transparent",
-				"border-color":"transparent"});
-		}
-	});
-
-/*HOME*/	
-	/*welcome*/
-	$('#welcome').hover(
-	    function() {
-	        var $this = $(this);
-	        $this.data('Bienvenid@', $this.text());
-	        $this.text("Welcome");
-	    },
-	    function() {
-	        var $this = $(this);
-	        $this.text($this.data('Benvenid@'));
-	    }
-	);
-	/*iAm*/
-	$('#iAm').hover(
-	    function() {
-	        var $this = $(this);
-	        $this.data('Soy Vanessa', $this.text());
-	        $this.text("I am Vann");
-	    },
-	    function() {
-	        var $this = $(this);
-	        $this.text($this.data('Soy Vanessa'));
-	    }
-	);
-	/*Diseño*/
-	$('#designer').hover(
-	    function() {
-	        var $this = $(this);
-	        $this.data('Diseñadora y', $this.text());
-	        $this.text("Designer &");
-	    },
-	    function() {
-	        var $this = $(this);
-	        $this.text($this.data('Diseñadora y'));
-	    }
-	);
-	/*web dev*/
-	$('#webDev').hover(
-	    function() {
-	        var $this = $(this);
-	        $this.data('Desarrolladora Web', $this.text());
-	        $this.text("Web Developer");
-	    },
-	    function() {
-	        var $this = $(this);
-	        $this.text($this.data('Desarrolladora Web'));
-	    }
-	);
-	/*Animals*/
-	$('#loveAnimals').hover(
-	    function() {
-	        var $this = $(this);
-	        $this.data('Amante de los animales', $this.text());
-	        $this.text("I love animals");
-	    },
-	    function() {
-	        var $this = $(this);
-	        $this.text($this.data('Amante de los animales'));
-	    }
-	);
-		/*movies*/
-	$('#loveMovies').hover(
-	    function() {
-	        var $this = $(this);
-	        $this.data('Los videojuegos y ver películas.', $this.text());
-	        $this.text("Video Games and Watch Movies :)");
-	    },
-	    function() {
-	        var $this = $(this);
-	        $this.text($this.data('Los videojuegos y ver películas.'));
-	    }
-	);
+noUiSlider.create(slider, {
+  start: [20, 80],
+  connect: true,
+  orientation: 'horizontal', // 'horizontal' or 'vertical'
+  range: {
+    'min': 0,
+    'max': 100
+  }
 });
+    });
+        
+
+// fin js first-filter//
+
+
